910a7a86bf11b0d405a165f93d143437676d24d2
Theja <chakripushpa10@gmail.com>
2026-09-03 11:36:04 +0530

    Second

---
+++ src/test/java/com/theja/StudentService/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theja.StudentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
